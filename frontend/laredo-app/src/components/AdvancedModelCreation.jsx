ba7ce2fbcb2bf5384b4c3a5e45ac891d7fecd7ba
iBerrouet <isaacberrouetsantos@gmail.com>
2024-04-09 22:03:37 +0200

    Add page layout without integrating data fetching

--- frontend/laredo-app/src/components/AdvancedModelCreation.jsx
+++ frontend/laredo-app/src/components/AdvancedModelCreation.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function AdvancedModelCreation() {
+
+    const [selectedAlgorithm, setSelectedAlgorithm] = useState("")
+
+    const handleSelectAlgorithm = (event) => {
+        setSelectedAlgorithm(event.target.value);
+    }
+
     return(
         <>
-            <p>Creacion de modelos avanzada</p>
+            <div className='grid grid-cols-2'>
+                <div>
+                    <div className='flex justify-center items-center mt-12'>
+                        <strong className='mr-4'>Algoritmo:</strong>
+                        <select className='text-white rounded border border-white bg-gray-800 w-60 h-6' value={selectedAlgorithm} onChange={handleSelectAlgorithm}>
+                            <option className='text-cyan-400 bg-gray-800 hover:text-white hover:bg-slate-950'>SVM</option>
+                            <option className='text-cyan-400 bg-gray-800 hover:text-white hover:bg-slate-950'>KNN</option>
+                        </select>
+                            
+                    </div>
+                    <div className="flex justify-center items-center mt-6">
+                        <p>Param</p>
+                        <p>Valor</p>
+                    </div>
+                </div>
+                
+                <div className='flex flex-col text-right mt-36 mr-28'>
+                    <h2 className='text-5xl font-bold'>Creación de modelos</h2>
+                    <h1 className='text-6xl font-bold'>Avanzada</h1>
+                    <p className='mt-5'>Establezca los parámetros del módelo una vez elegido el algoritmo.</p>
+                </div>
+            </div>
         </>
     )
 }
