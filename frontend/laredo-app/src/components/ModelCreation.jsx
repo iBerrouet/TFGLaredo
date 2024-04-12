8f744f182e48325095bd690fd8c91ff1d4677931
iBerrouet <isaacberrouetsantos@gmail.com>
2024-04-12 14:09:52 +0200

    Add navigation bar layout

---
+++ frontend/laredo-app/src/components/ModelCreation.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import CustomButton from './CustomButton'
+
+const Steps = {
+    Dataset: 'Dataset',
+    Preprocessing: 'Preprocessing',
+    Algorithm: 'Algorithm',
+    Evaluation: 'Evaluation'
+}
+
+
+function ModelCreation() {
+    const [activeButton, setActiveButton] = useState("Dataset")
+    const navigate = useNavigate()
+
+    const goHome = () => {
+        navigate('/')
+    }
+
+    const goModels = () => {
+        navigate('/models')
+    }
+
+    const handleActive = (step) => {
+        setActiveButton(step)
+    }
+
+    return(
+        <>
+            <header className='bg-gray-800 h-20 flex items-center'>
+                <strong className='text-3xl font-bold italic text-white ml-10 cursor-pointer' onClick={goHome}>LAREDO</strong>
+                <CustomButton className='ml-auto mr-10' onClick={goModels}>Mostrar modelos disponibles</CustomButton>
+            </header>
+            <div className='flex justify-center mt-3'>
+                <div className='inline-flex w-3/4'>
+                    <button className={`flex-1 ${activeButton == Steps.Dataset 
+                        ? 'bg-transparent text-cyan-400 text-lg py-2 w-40 border-2 border-r rounded-l' 
+                        : 'bg-gray-900 hover:bg-transparent hover:text-cyan-400 text-white text-lg font-bold py-2 w-40 border-2 rounded-l-lg border-r'}`}
+                        onClick={() => handleActive(Steps.Dataset)}>
+                        {Steps.Dataset}
+                    </button>
+                    <button className={`flex-1 ${activeButton == Steps.Preprocessing 
+                        ? 'bg-transparent text-cyan-400 text-lg py-2 w-40 border-2 border-l border-r' 
+                        : 'bg-gray-900 hover:bg-transparent hover:text-cyan-400 text-white text-lg font-bold py-2 w-40 border-2 border-l border-r'}`}
+                        onClick={() => handleActive(Steps.Preprocessing)}>
+                        {Steps.Preprocessing}
+                    </button>
+                    <button className={`flex-1 ${activeButton == Steps.Algorithm 
+                        ? 'bg-transparent text-cyan-400 text-lg py-2 w-40 border-2 border-l border-r' 
+                        : 'bg-gray-900 hover:bg-transparent hover:text-cyan-400 text-white text-lg font-bold py-2 w-40 border-2 border-l border-r'}`}
+                        onClick={() => handleActive(Steps.Algorithm)}>
+                        {Steps.Algorithm}
+                    </button>
+                    <button className={`flex-1 ${activeButton == Steps.Evaluation 
+                        ? 'bg-transparent text-cyan-400 text-lg py-2 w-40 border-2 rounded-r-lg border-l' 
+                        : 'bg-gray-900 hover:bg-transparent hover:text-cyan-400 text-white text-lg font-bold py-2 w-40 border-2 rounded-r-lg border-l'}`}
+                        onClick={() => handleActive(Steps.Evaluation)}>
+                        {Steps.Evaluation}
+                    </button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ModelCreation
