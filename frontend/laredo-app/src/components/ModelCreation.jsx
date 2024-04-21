d3cc93eb668fa884c441fb4b3ee3cb1a64cd0417
iBerrouet <isaacberrouetsantos@gmail.com>
2024-04-21 15:43:15 +0200

    Select dataset by default in ModelCreation

--- frontend/laredo-app/src/components/ModelCreation.jsx
+++ frontend/laredo-app/src/components/ModelCreation.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import { useNavigate, Route, Routes } from 'react-router-dom'
 import CustomButton from './CustomButton'
 import DatasetPreprocessing from './DatasetPreprocessing'
@@ -14,12 +15,11 @@ const Steps = {
 
 
 function ModelCreation() {
-    const [activeButton, setActiveButton] = useState('')
+    const [activeButton, setActiveButton] = useState(Steps.Dataset)
     const navigate = useNavigate()
 
     const goHome = () => {
         navigate('/')
-
     }
 
     const goModels = () => {
@@ -34,25 +34,9 @@ function ModelCreation() {
         <>
             <header className='bg-gray-800 h-20 flex items-center'>
                 <strong className='text-3xl font-bold italic text-white ml-10 cursor-pointer' onClick={goHome}>LAREDO</strong>
-                <CustomButton className='ml-auto mr-10' onClick={goModels}>Mostrar modelos disponibles</CustomButton>
+                <CustomButton className='ml-auto mr-10' onClick={goModels}>Show available models</CustomButton>
             </header>
 
-            <div className='flex justify-center items-center mt-12'>
-            <button onClick={() => setShowAdvance(false)} className={ showAdvance 
-                    ? 'bg-gray-900 hover:bg-transparent hover:text-cyan-400 text-white text-[16px] py-2 w-40 border-2 border-r rounded-l'
-                    : 'bg-transparent text-cyan-400 text-[16px] py-2 w-40 border-2 border-r rounded-l'}>
-                    Basica
-                </button>
-                <button onClick={() => setShowAdvance(true)} className={showAdvance 
-                    ? 'bg-transparent text-cyan-400 text-[16px] py-2 w-40 border-2 border-l rounded-r'
-                    : 'bg-gray-900 hover:bg-transparent hover:text-cyan-400 text-white text-[16px] py-2 w-40 border-2 border-l rounded-r'}>
-                    Avanzada
-                </button>
-
-            </div>
-
-            {showAdvance ? <AdvancedModelCreation /> : <BasicModelCreation />}
-                <CustomButton className='ml-auto mr-10' onClick={goModels}>Show available models</CustomButton>
             <div className='flex justify-center mt-3'>
                 <div className='inline-flex w-3/4'>
                     <button className={`flex-1 ${activeButton == Steps.Dataset 
