47c4eb0ff61fa79f8526a3a0589ce20a5331f6ba
iBerrouet <isaacberrouetsantos@gmail.com>
2024-05-06 12:00:42 +0200

    Train a model example

--- backend/mlflow_api.py
+++ backend/mlflow_api.py
@@ -1,8 +1,13 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import mlflow
 from flask_restful import Api
 from flask_cors import CORS
+import pandas as pd
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import f1_score, recall_score
+from sklearn.model_selection import train_test_split
 
 
 app = Flask(__name__)
@@ -45,6 +50,74 @@ def get_model(model_name):
 
     return jsonify(response_data), 200
     
+@app.route('/train_model', methods=['POST'])
+def train_model():
+
+    data = request.json
+
+    dataset_json = data.get('datasetJSON')
+    columns_data_type = data.get('columnsDataType')
+    algorithm = data.get('algorithm')
+    parameters_value = data.get('parametersValue')
+
+    dataset = pd.DataFrame.from_dict(dataset_json)
+    dataset.loc[((dataset.machine_status == 'BROKEN') | (dataset.machine_status == 'RECOVERING')), 'machine_status'] = 'BROKEN'
+
+    dataset = dataset.drop(columns=['sensor_15', '', 'timestamp'])
+
+    dataset = dataset[~dataset.map(lambda x: x == "").any(axis=1)]  
+
+    x = dataset.drop(columns=['machine_status'])
+    y = dataset['machine_status']
+
+    #kfold cross validation
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+    #training = mlflow.data.from_pandas(_,targets='label')
+
+
+    with mlflow.start_run():
+
+        #mlflow.log_input(training, "training")
+
+
+        for key, value in parameters_value.items():
+            print(key + ': ' + str(type(value)))
+
+        if algorithm == "random_forest" :
+            model = RandomForestClassifier(**parameters_value)
+        elif algorithm == "decision_tree" :
+            model = tree.DecisionTreeClassifier(**parameters_value)
+        else:
+            return jsonify({"message": "Invalid algorithm"}), 400
+
+        model.fit(x_train, y_train)
+
+        predictions = model.predict(x_test)
+
+        accuracy = model.score(x_test, y_test)
+
+        tpr = recall_score(y_test, predictions, pos_label='BROKEN')
+        fpr = 1 - recall_score(y_test, predictions, pos_label='NORMAL')
+        f1 = f1_score(y_test, predictions, pos_label='BROKEN')
+
+        #sklearn.utils.estimator_html_repr
+
+
+        #model_evaluation.html sklearn
+        mlflow.log_param("algorithm", algorithm)
+        mlflow.log_params(parameters_value)
+        mlflow.log_metric('accuracy', accuracy)
+        mlflow.log_metric('tpr', tpr)
+        mlflow.log_metric('fpr', fpr)
+        mlflow.log_metric('f1_score', f1)
+
+
+        mlflow.sklearn.log_model(sk_model=model, artifact_path="model", registered_model_name="model_name")
+
+
+    return jsonify({"message": "Model trained successfully"}), 200
+
 
 if __name__ == "__main__":
     app.run(port=5050)
