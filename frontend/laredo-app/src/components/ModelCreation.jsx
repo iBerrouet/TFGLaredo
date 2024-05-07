aa936ccb67018bf5d7ee571270646a0d602fb446
iBerrouet <isaacberrouetsantos@gmail.com>
2024-05-07 21:39:29 +0200

    Implement layout for user input of model name and problem type selection

--- frontend/laredo-app/src/components/ModelCreation.jsx
+++ frontend/laredo-app/src/components/ModelCreation.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { useNavigate, Route, Routes } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, Route, Routes, useAsyncError } from 'react-router-dom'
 import CustomButton from './CustomButton'
 import DatasetPreprocessing from './DatasetPreprocessing'
 import DatasetUploading from './DatasetUploading'
@@ -7,6 +7,8 @@ import ModelSelection from './ModelSelection'
 import ModelEvaluation from './ModelEvaluation'
 import Papa from 'papaparse'
 import axios from 'axios'
+import algorithmData from '../assets/algorithmParameters.json'
+
 
 const Steps = {
     Dataset: 'Dataset',
@@ -17,7 +19,7 @@ const Steps = {
 
 
 function ModelCreation() {
-    const [activeButton, setActiveButton] = useState(Steps.Dataset)
+    const [activeButton, setActiveButton] = useState("")
     const [datasetFile, setDatasetFile] = useState(null)
     const [columnsDataType, setColumnsDataType] = useState({})
     const [preview, setPreview] = useState([])
@@ -28,10 +30,20 @@ function ModelCreation() {
     const [algorithm, setAlgorithm] = useState("")
     const [parametersValue, setParametersValue] = useState({})
 
+    const [modelName, setModelName] = useState("")
+    const [problemType, setProblemType] = useState("")
+    const [hasStarted, setHasStarted] = useState(false)
+    const [problemTypes, setProblemTypes] = useState([])
+
     const navigate = useNavigate()
 
     const activeButtonStyle = 'bg-transparent text-cyan-400 font-bold text-lg py-2 w-40'
     const inactiveButtonStyle = 'bg-gray-900 hover:bg-transparent hover:text-cyan-400 text-white text-lg  py-2 w-40'
+
+    useEffect(() => {
+        const uniqueProblemTypes = Array.from(new Set(Object.values(algorithmData).map(item => item.problem_type)))
+        setProblemTypes(uniqueProblemTypes)
+    }, [])
 
     const goHome = () => {
         navigate('/')
@@ -100,34 +112,69 @@ function ModelCreation() {
                 <strong className='text-3xl font-bold italic text-white ml-10 cursor-pointer' onClick={goHome}>LAREDO</strong>
                 <CustomButton className='ml-auto mr-10' onClick={goModels}>Show available models</CustomButton>
             </header>
-            <div className='flex justify-center mt-3'>
-                <div className='inline-flex w-3/4'>
-                <button className={`flex-1 border-2 rounded-l-lg ${activeButton === Steps.Dataset 
-                        ? activeButtonStyle 
-                        : inactiveButtonStyle}`}
-                        onClick={() => handleActive(Steps.Dataset)}>
-                        {Steps.Dataset}
-                    </button>
-                    <button className={`flex-1 border-2 border-l-0 ${activeButton === Steps.Preprocessing 
-                        ? activeButtonStyle 
-                        : inactiveButtonStyle}`}
-                        onClick={() => handleActive(Steps.Preprocessing)}>
-                        {Steps.Preprocessing}
-                    </button>
-                    <button className={`flex-1 border-2 border-l-0 ${activeButton === Steps.Algorithm 
-                        ? activeButtonStyle 
-                        : inactiveButtonStyle}`}
-                        onClick={() => handleActive(Steps.Algorithm)}>
-                        {Steps.Algorithm}
-                    </button>
-                    <button className={`flex-1 border-2 border-l-0 rounded-r-lg ${activeButton === Steps.Evaluation 
-                        ? activeButtonStyle 
-                        : inactiveButtonStyle}`}
-                        onClick={() => handleActive(Steps.Evaluation)}>
-                        {Steps.Evaluation}
-                    </button>
+            { !hasStarted ?
+                <div className='flex flex-col items-center justify-center'>
+                    <h1 className='text-7xl font-bold text-center mt-12'>Welcome to model creation</h1>
+                    <p className='text-xl text-center mt-6'>Start by defining your model's name and selecting its problem type</p>
+                    <div className="flex flex-col items-start justify-center mt-12">
+                        
+                        <label htmlFor="modelName" className="text-2xl text-white mb-1">Name</label>
+                        
+                        <input 
+                            id="modelName"
+                            className='border border-white rounded bg-gray-800 text-lg text-white p-1 h-9 w-64'
+                            type="text" 
+                            placeholder="Enter your model's name..."
+                            value={modelName}
+                            onChange={(event) => setModelName(event.target.value)}
+                        />
+
+                        <label htmlFor="problemType" className="text-2xl text-white mb-1 mt-8">Problem type</label>
+                        <select 
+                            id="problemType" 
+                            className='text-lg text-white rounded border border-white bg-gray-800 p-1 h-9 w-64'
+                            value={problemType}
+                            onChange={(event) => setProblemType(event.target.value)}
+                        >
+                            <option className='text-lg' value="">Select a problem type...</option>
+                            {problemTypes.map((problemType, index) => (
+                                <option className='text-lg' key={index} value={problemType}>{problemType}</option>
+                            ))}
+                        </select>
+                    </div>
+
+                    <CustomButton onClick={() => setHasStarted(true)} className='mt-16'>Start</CustomButton>
+                </div>                    
+            :
+                <div className='flex justify-center mt-3'>
+                    <div className='inline-flex w-3/4'>
+                    <button className={`flex-1 border-2 rounded-l-lg ${activeButton === Steps.Dataset 
+                            ? activeButtonStyle 
+                            : inactiveButtonStyle}`}
+                            onClick={() => handleActive(Steps.Dataset)}>
+                            {Steps.Dataset}
+                        </button>
+                        <button className={`flex-1 border-2 border-l-0 ${activeButton === Steps.Preprocessing 
+                            ? activeButtonStyle 
+                            : inactiveButtonStyle}`}
+                            onClick={() => handleActive(Steps.Preprocessing)}>
+                            {Steps.Preprocessing}
+                        </button>
+                        <button className={`flex-1 border-2 border-l-0 ${activeButton === Steps.Algorithm 
+                            ? activeButtonStyle 
+                            : inactiveButtonStyle}`}
+                            onClick={() => handleActive(Steps.Algorithm)}>
+                            {Steps.Algorithm}
+                        </button>
+                        <button className={`flex-1 border-2 border-l-0 rounded-r-lg ${activeButton === Steps.Evaluation 
+                            ? activeButtonStyle 
+                            : inactiveButtonStyle}`}
+                            onClick={() => handleActive(Steps.Evaluation)}>
+                            {Steps.Evaluation}
+                        </button>
+                    </div>
                 </div>
-            </div>
+            }
             {activeButton === Steps.Dataset && 
                 <DatasetUploading 
                     datasetFile={datasetFile}
@@ -156,6 +203,7 @@ function ModelCreation() {
                 />
             }
             {activeButton === Steps.Evaluation && <ModelEvaluation />}
+            
         </>
     )
 }
