5b0b54c43abcc73717e23b10f5659939f66cff7d
iBerrouet <isaacberrouetsantos@gmail.com>
2024-05-12 21:47:40 +0200

    Add error handling for parameter values in preprocessing method selection

--- frontend/laredo-app/src/components/DatasetPreprocessing.jsx
+++ frontend/laredo-app/src/components/DatasetPreprocessing.jsx
@@ -2,24 +2,29 @@ import React, { useState } from 'react'
 import CustomButton from './CustomButton'
 import CustomModal from './CustomModal'
 import preprocessingMethods from '../assets/preprocessingMethods.json'
+import { validateAndParseParam } from '../utils/paramsUtils'
 
 function DatasetPreprocessing({selectedMethods, setSelectedMethods, onNextStep}) {
 
     const [showModal, setShowModal] = useState(false)
+    const [selectedCategory, setSelectedCategory] = useState('')
     const [selectedMethod, setSelectedMethod] = useState('')
     const [selectedParams, setSelectedParams] = useState({})
+    const [errors, setErrors] = useState({})
 
     const openModal = () => {
         setShowModal(true)
     }
 
     const closeModal = () => {
+        setErrors({})
         setShowModal(false)
     }
 
-    const handleCellClick = (method, params) => {
+    const handleCellClick = (category, method, params) => {
         if (Object.keys(params).length > 0) {
             openModal()
+            setSelectedCategory(category)
             setSelectedMethod(method)
             setSelectedParams(params)
         } else {
@@ -28,12 +33,36 @@ function DatasetPreprocessing({selectedMethods, setSelectedMethods, onNextStep})
     }
 
     const applyParams = () => {
-        setSelectedMethods({ ...selectedMethods, ['minmax scaler']: selectedParams })
-        closeModal()
+        const validatedParams = {}
+        let hasError = false
+        const methodParams = preprocessingMethods[selectedCategory][selectedMethod]
+
+        Object.entries(selectedParams).forEach(([paramName, value]) => {
+            const param = methodParams[paramName]
+            const { isValidParameter, parsedValue } = validateAndParseParam(paramName, value, param.type, param.enum)
+            if (!isValidParameter) {
+                setErrors(prevErrors => ({
+                    ...prevErrors,
+                    [paramName]: 'Input value'
+                }))
+                hasError = true
+            } else {
+                validatedParams[paramName] = parsedValue
+            }
+        })
+
+        if (!hasError) {
+            setSelectedMethods({ ...selectedMethods, [selectedMethod]: validatedParams })
+            closeModal()
+        }
     }
 
     const handleChange = (paramName, value) => {
-        setSelectedParams({ ...selectedParams, [paramName]: value });
+        setSelectedParams({ ...selectedParams, [paramName]: value })
+        setErrors(prevErrors => ({
+            ...prevErrors,
+            [paramName]: ''
+        }))
     }
 
     return(
@@ -42,8 +71,8 @@ function DatasetPreprocessing({selectedMethods, setSelectedMethods, onNextStep})
                 <h1 className='text-6xl font-bold mt-8'>Customize preprocessing pipeline</h1>
                 <strong className='mt-5'>Click on the techniques you want to use and specify the parameters to construct your pipeline.</strong>
                 <div className='grid grid-cols-2 w-3/4'>
-                <table className='border border-white mt-5 w-fit mx-auto'>
-                    {Object.entries(preprocessingMethods).map(([category, methods]) => (
+                    <table className='border border-white mt-5 w-fit mx-auto'>
+                        {Object.entries(preprocessingMethods).map(([category, methods]) => (
                             <React.Fragment key={category}>
                                 <tbody>
                                     <tr>
@@ -53,7 +82,7 @@ function DatasetPreprocessing({selectedMethods, setSelectedMethods, onNextStep})
                                         <tr key={method}>
                                             <td
                                                 className='px-5 py-1 bg-gray-800 hover:bg-transparent cursor-pointer'
-                                                onClick={() => handleCellClick(method, params)}
+                                                onClick={() => handleCellClick(category, method, params)}
                                             >
                                                 {method}
                                             </td>
@@ -97,6 +126,7 @@ function DatasetPreprocessing({selectedMethods, setSelectedMethods, onNextStep})
                             className='text-xl border border-white bg-gray-800 rounded-md p-2'
                             onChange={(e) => handleChange(paramName, e.target.value)}
                         />
+                        <strong className='text-red-500'>{errors[paramName]}</strong>
                     </div>
                 ))}
                 <CustomButton className='mt-6' onClick={applyParams}>Apply</CustomButton>
